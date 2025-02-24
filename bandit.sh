3d973186d5f1a71c6877fd96cda8127b34e1618e
RHallanger <rjhallanger@students.nic.edu>
2025-02-23 20:48:39 -0800

    Update bandit.sh
    Had some uppercase e's lounging about the place and a missing $.

--- bandit.sh
+++ bandit.sh
@@ -2,11 +2,11 @@
 read -p "Select a level (0-34): " gameLevel
 
 #Due to the wargames requiring a signature file for access after level 13, we will make separate commands for the set...
-if [ "$gameLEvel" -gt 13 ]; then
+if [ "$gameLevel" -gt 13 ]; then
   echo "You have selected level $gameLevel which requires a key present."
   ssh -i sshkey.private bandit$gameLevel@bandit.labs.overthewire.org -p 2220
 
-elif [ "$gameLEvel" -lt 14 ] && [ "gameLevel" -ne 0 ]; then
+elif [ "$gameLevel" -lt 14 ] && [ "$gameLevel" -ne 0 ]; then
   echo "Connecting to Bandit Level $gameLevel."
   ssh bantit$gameLevel@bandit.labs.overthewire.org -p 2220
 
