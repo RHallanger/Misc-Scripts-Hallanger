ba45a255a275d45bdc85a44b4973e0ac28285af4
RHallanger <rjhallanger@students.nic.edu>
2025-04-07 08:02:33 -0700

    Update OMP_LINUX.sh

--- OMP_LINUX.sh
+++ OMP_LINUX.sh
@@ -5,15 +5,15 @@ USHELL=none
 INC=0
 
 #This while loop will search the SHELL env var path until it lands on Z Shell or BASH with up to 10 directories in path.
-while (([ "$USHELL" != "zsh" ] && [ "$USHELL" != "bash" ]) && [ $INC -le 9 ]);
+while (([ "$USHELL" != "zsh" ] && [ "$USHELL" != "bash" ]) && [ "$INC" -le "9" ]);
 do
 	USHELL=$(echo $SHELL | awk -F'/' -v increment=$INC '{print $increment}')
-	echo "USHELL: $USHELL           Loop Count: $INC"
+	echo "USHELL: $USHELL | Loop Count: $INC"
 	INC=$((INC + 1))
 done
 
 #If the shell check was performed but the increment is or exceeds 10 it will close the program.
-if [ $INC -ge 10 ];
+if [ "$INC" -ge "10" ];
 then
 	echo "Your shell is not supported."
 	echo "Closing the program..."
@@ -28,7 +28,9 @@ then
 	echo "alias oh-my-posh='~/.local/bin/oh-my-posh'" >> ~/.bashrc
 	echo "OhMyPOSH has been aliased into .bashrc"
 	source ~/.bashrc
+	. ~/.bashrc
 	echo "OhMyPOSH has been initialized into BASH."
+	wait
 elif [ "$USHELL" == "zsh" ];
 then
 	echo "Z Shell detected..."
@@ -36,30 +38,34 @@ then
 	echo "alias oh-my-posh='~/.local/bin/oh-my-posh'" >> ~/.zshrc
 	echo "OhMyPosh has been aliased into .zshrc"
 	source ~/.zshrc
+	. ~/.zshrc
 	echo "OhMyPosh has been aliased into Z Shell."
-else
-	echo "ERROR: 000001 - SHELL CHECK BYPASSED!!!!!" >> ~/ompscript.log
-	exit 1
+	wait
 fi
 
 echo "Installing the HACK Nerd Font..."
+echo "If there is a missing oh-my-posh command error, please use <bash -i OMP_LINUX.sh>."
 oh-my-posh font install hack
+wait
 
 if [ "$USHELL" == "bash" ];
 then
 	echo "BASH detected..."
 	echo "eval \"\$(oh-my-posh init bash --config ~/.cache/oh-my-posh/themes/tokyo.omp.json)\"" >> ~/.bashrc
 	source ~/.bashrc
+	. ~/.bashrc
+	wait
 elif [ "$USHELL" == "zsh" ];
 then
 	echo "ZSH detected..."
 	echo "eval \"\$(oh-my-posh init bash --config ~/.cache/oh-my-posh/themes/tokyo.omp.json)\"" >> ~/.zshrc
 	source ~/.zshrc
-else
-	echo "ERROR: 000002 EVAL BYPASS!!!!!" >> ~/ompscript.log
-	exit 2
+	. ~/.zshrc
+	wait
 fi
 
 echo "OhMyPOSH successfully installed..."
 source ~/.bashrc
+. ~/.bashrc
+wait
 exit 3
