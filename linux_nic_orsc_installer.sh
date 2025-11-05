b7ae759e24b6f938f0ef346592e5b9ba4549276a
RHallanger <188368293+RHallanger@users.noreply.github.com>
2025-11-05 13:26:22 -0800

    Update Java installation command in installer script

--- linux_nic_orsc_installer.sh
+++ linux_nic_orsc_installer.sh
@@ -2,7 +2,7 @@
 dnf update
 dnf upgrade
 dnf install wget
-dnf install java-17-openjdk-1:17.0.17.0.10-1.el9.x86_64
+dnf install java -y
 wget http://10.0.17.77:8000/rsa_key -P $HOME/Downloads
 wget https://github.com/RSCPlus/rscplus/releases/download/Latest/rscplus.jar -P $HOME/Downloads
 echo 'alias orsc="java -jar $HOME/Downloads/rscplus.jar"' >> $HOME/.bashrc
